749e6441d7d451173f475493364d6e77b1acce13
WonSeo <121783811+qlalfdmlghk1@users.noreply.github.com>
2026-02-03 02:01:05 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 3000 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,101 +1,90 @@
-from collections import deque
-import copy
-import sys
-input = sys.stdin.readline
-
-r,c,T = map(int, input().split())
-grid = []
+r,c,t = map(int,input().split())
+graph = []
 
 for _ in range(r) :
-    grid.append(list(map(int,input().split())))
-
-air_cleaner = []
-
-for i in range(r) :
-    if grid[i][0] == -1 :
-        air_cleaner.append(i)
-    if len(air_cleaner) == 2 :
-        break
-
-upper_air_cleaner = air_cleaner[0]
-lower_air_cleaner = air_cleaner[1]
+    graph.append(list(map(int,input().split())))
 
 dr = [1,-1,0,0]
 dc = [0,0,1,-1]
 
-def dust(cur_r,cur_c) :
-    global new_grid
-    spread = grid[cur_r][cur_c] // 5
-    cnt = 0
+# 공기청정기 위치
+for i in range(r) :
+    if graph[i][0] == -1 :
+        a1 = i
+        break
+a2 = i + 1
+
+# 미세먼지 확산 함수
+def spread(new_graph,cr,cc) :
+    cnt = 0  # 확산된 방향의 개수
     for i in range(4) :
-        nex_r, nex_c = cur_r + dr[i], cur_c + dc[i]
-        if 0 <= nex_r < r and 0 <= nex_c < c and grid[nex_r][nex_c] != -1 :
-            new_grid[nex_r][nex_c] += spread
+        nr, nc = cr + dr[i], cc + dc[i]
+        if 0 <= nr < r and 0 <= nc < c and graph[nr][nc] != -1 :
             cnt += 1
-    new_grid[cur_r][cur_c] += grid[cur_r][cur_c] - (spread * cnt)
+            new_graph[nr][nc] += graph[cr][cc] // 5
+    new_graph[cr][cc] += graph[cr][cc] - ((graph[cr][cc] // 5) * cnt)
+
+# 공기청정기 작동
+def cleaning(new_graph,a1,a2) :
+    # 윗 회전 (반시계)
+    up_list = []
+    for j in range(c-1) :
+        up_list.append(new_graph[a1][j])
+    for i in range(a1,0,-1) :
+        up_list.append(new_graph[i][c-1])
+    for j in range(c-1,0,-1) :
+        up_list.append(new_graph[0][j])
+    for i in range(0,a1) :
+        up_list.append(new_graph[i][0])
+
+    up_list.pop(-1)
+
+    for j in range(1,c-1) :
+        new_graph[a1][j] = up_list.pop(0)
+    for i in range(a1,0,-1) :
+        new_graph[i][c-1] = up_list.pop(0)
+    for j in range(c-1,0,-1) :
+        new_graph[0][j] = up_list.pop(0)
+    for i in range(0,a1) :
+        new_graph[i][0] = up_list.pop(0)
 
 
-def air_clean() :
-    global grid
-    global air_cleaner
-    q1 = deque()
-    q1.append(0)
-    for j in range(1, c):
-        q1.append(grid[upper_air_cleaner][j])
-    for i in range(upper_air_cleaner-1, -1, -1):
-        q1.append(grid[i][c-1])
-    for j in range(c-2, -1, -1):
-        q1.append(grid[0][j])
-    for i in range(1, upper_air_cleaner-1):
-        q1.append(grid[i][0])
-    # print(q1)
+    # 아랫 회전 (반시계)
+    down_list = []
+    for j in range(c-1) :
+        down_list.append(new_graph[a2][j])
+    for i in range(a2,r-1) :
+        down_list.append(new_graph[i][c-1])
+    for j in range(c-1,0,-1) :
+        down_list.append(new_graph[r-1][j])
+    for i in range(r-1,a2,-1) :
+        down_list.append(new_graph[i][0])
 
-    for j in range(1, c):
-        grid[upper_air_cleaner][j] = q1.popleft()
-    for i in range(upper_air_cleaner-1,-1,-1):
-        grid[i][c-1] = q1.popleft()
-    for j in range(c-2,-1,-1):
-        grid[0][j] = q1.popleft()
-    for i in range(1, upper_air_cleaner):
-        grid[i][0] = q1.popleft()
+    down_list.pop(-1)
 
-    q2 = deque()
-    q2.append(0)
-    for j in range(1,c) :
-        q2.append(grid[lower_air_cleaner][j])
-    for i in range(lower_air_cleaner+1,r) :
-        q2.append(grid[i][c-1])
-    for j in range(c-2,-1,-1) :
-        q2.append(grid[r-1][j])
-    for i in range(r-2,lower_air_cleaner+1,-1) :
-        q2.append(grid[i][0])
-    # print(q2)
+    for j in range(1,c-1) :
+        new_graph[a2][j] = down_list.pop(0)
+    for i in range(a2,r-1) :
+        new_graph[i][c-1] = down_list.pop(0)
+    for j in range(c-1,0,-1) :
+        new_graph[r-1][j] = down_list.pop(0)
+    for i in range(r-1,a2,-1) :
+        new_graph[i][0] = down_list.pop(0)
 
-    for j in range(1,c) :
-        grid[lower_air_cleaner][j] = q2.popleft()
-    for i in range(lower_air_cleaner+1,r) :
-        grid[i][c-1] = q2.popleft()
-    for j in range(c-2,-1,-1) :
-        grid[r-1][j] = q2.popleft()
-    for i in range(r-2,lower_air_cleaner,-1) :
-        grid[i][0] = q2.popleft()
-
-
-for t in range(T) :
-    new_grid = [[0 for _ in range(c)] for _ in range(r)]
-    for i in range(r) :
+for _ in range(t) :
+    new_graph = [[0 for _ in range(c)]  for _ in range(r)]
+    for i in range(r) : 
         for j in range(c) :
-            if grid[i][j] > 0 :
-                dust(i,j)
-    grid = copy.deepcopy(new_grid)
-    air_clean()
-    grid[upper_air_cleaner][0] = -1
-    grid[lower_air_cleaner][0] = -1
+            if graph[i][j] > 0 :
+                spread(new_graph,i,j)
 
-# print(grid)
-answer = 0
-for i in range(r):
-    for j in range(c):
-        if grid[i][j] > 0:  # 공기청정기(-1) 제외
-            answer += grid[i][j]
+    cleaning(new_graph,a1,a2)
+    graph = new_graph
+    graph[a1][0],graph[a2][0] = -1,-1
+
+answer = 2
+
+
+for g in graph :
+    answer += sum(g)
 print(answer)
